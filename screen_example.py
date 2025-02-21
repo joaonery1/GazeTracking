c91d9ce302a822c96a69eab338b68fa08b3d6223
joaonery <joao.nery1@hotmail.com>
2025-02-21 10:57:49 -0300

    screen flow adjustment

--- screen_example.py
+++ screen_example.py
@@ -12,6 +12,8 @@ screen = screeninfo.get_monitors()[0]
 screen_width = screen.width
 screen_height = screen.height
 screen_overlay = np.zeros((screen_height, screen_width, 3), dtype=np.uint8)
+frame_overlay = screen_overlay.copy()
+calibrated = False
 
 while True:
 
@@ -20,14 +22,16 @@ while True:
         # Analisa a direção do olhar e mapeia para as coordenadas da tela
         screen_x, screen_y = epog.analyze(frame)
 
-        # Copia do overlay da tela para exibir o ponto de foco
-        frame_overlay = screen_overlay.copy()
+        if screen_x is not None and screen_y is not None and not calibrated:
+            calibrated = True
+            print("Calibração concluída com sucesso!!")
 
-        cv2.circle(frame_overlay, (screen_x,  screen_y), 10, (0, 0, 255), -1)
-        cv2.circle(frame_overlay, (screen_x,  screen_y), 150, (0, 0, 255), 2)
-        cv2.imshow("Screen Overlay", frame_overlay)
-        #cv2.circle(frame_overlay, (screen_x, screen_y), 20, (0, 255, 0), -1)
-
+        if calibrated:
+            # Exibe tela após calibração
+            cv2.circle(frame_overlay, (screen_x,  screen_y), 10, (0, 0, 255), -1)
+            cv2.circle(frame_overlay, (screen_x,  screen_y), 150, (0, 0, 255), 2)
+            cv2.imshow("Screen Overlay", frame_overlay)
+        
         if cv2.waitKey(1) == 27:
             epog.webcam.release()
             cv2.destroyAllWindows()
